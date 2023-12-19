8c68c32d1f1a4e46296b5cb0f04267eabe309e05
Alaa <aalaaeiid@gmail.com>
2023-12-19 15:19:14 +0200

    weatherApp init

---
+++ WeatherNow/WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Alaa Eid on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
