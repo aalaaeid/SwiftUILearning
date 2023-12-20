b4c3db1170b867ac419a22f347218d2d4b1983f6
Alaa <aalaaeiid@gmail.com>
2023-12-20 15:32:14 +0200

    setup coffee app with models and service

---
+++ GetMeCoffee/GetMeCoffee/GetMeCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMeCoffeeApp.swift
+//  GetMeCoffee
+//
+//  Created by Alaa Eid on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GetMeCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
